ce2bb59a5b8b541239e39b9e1e95d2f780f31ae7
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2018-10-22 21:21:46 +0100

    Example code from discord.py site which reads token from file

---
+++ tabutron.py
@@ -0,0 +1,30 @@
+import boto3
+import discord
+import asyncio
+
+client = discord.Client()
+with open("token.txt", "r") as token_file:
+    token = token_file.read()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.run(token)
